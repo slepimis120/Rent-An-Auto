bff9a8f8d9c2ae01ad58dadfc1392200d36ceb4a
slepimis120 <slepimis120@gmail.com>
2026-06-10 22:00:37 +0200

    feat: Implement Docker for Shop frontend

--- payment-provider/payment-provider-backend/src/main/java/com/rentauto/paymentprovider/service/MerchantService.java
+++ payment-provider/payment-provider-backend/src/main/java/com/rentauto/paymentprovider/service/MerchantService.java
@@ -1,17 +1,19 @@
 package com.rentauto.paymentprovider.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.rentauto.paymentprovider.api.dto.RegisterRequest;
 import com.rentauto.paymentprovider.api.dto.RegisterResponse;
 import com.rentauto.paymentprovider.api.mapper.MerchantMapper;
 import com.rentauto.paymentprovider.domain.Merchant;
 import com.rentauto.paymentprovider.domain.User;
 import com.rentauto.paymentprovider.repository.MerchantRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
@@ -35,7 +37,10 @@ public class MerchantService {
         merchant.setPassword(passwordEncoder.encode(request.password()));
         merchant.setRole(User.Role.ROLE_MERCHANT);
 
+        merchant.setMerchantApiKey(UUID.randomUUID().toString());
+
         Merchant savedMerchant = repository.save(merchant);
+
         return merchantMapper.toRegisterResponse(savedMerchant);
     }
 
