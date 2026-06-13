a602cce32016c81248e165159ccc23f808c41578
slepimis120 <slepimis120@gmail.com>
2026-06-13 18:46:30 +0200

    start working on QR code payment

---
+++ payment-provider/payment-provider-backend/src/main/java/com/rentauto/paymentprovider/plugins/qr/QrCodePaymentController.java
@@ -0,0 +1,28 @@
+package com.rentauto.paymentprovider.plugins.qr;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rentauto.paymentprovider.api.dto.PaymentInitRequest;
+import com.rentauto.paymentprovider.api.dto.PaymentInitResponse;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/payments/qr")
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:9090")
+public class QrCodePaymentController {
+
+    private final QrCodePaymentPlugin qrPlugin;
+
+    @PostMapping("/process")
+    public ResponseEntity<PaymentInitResponse> processQrPayment(@RequestBody PaymentInitRequest request) {
+        PaymentInitResponse tx = qrPlugin.processPayment(request);
+        return ResponseEntity.ok(tx);
+    }
+}
--- payment-provider/payment-provider-backend/src/main/java/com/rentauto/paymentprovider/plugins/qr/QrCodePaymentPlugin.java
+++ payment-provider/payment-provider-backend/src/main/java/com/rentauto/paymentprovider/plugins/qr/QrCodePaymentPlugin.java
@@ -1,20 +1,21 @@
 package com.rentauto.paymentprovider.plugins.qr;
 
-import com.rentauto.paymentprovider.api.dto.PaymentInitRequest;
-import com.rentauto.paymentprovider.api.dto.PaymentInitResponse;
-import com.rentauto.paymentprovider.domain.PaymentMethod;
-import com.rentauto.paymentprovider.domain.PaymentStatus;
-import com.rentauto.paymentprovider.domain.Transaction;
-import com.rentauto.paymentprovider.plugins.PaymentPlugin;
-import lombok.AllArgsConstructor;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import com.rentauto.paymentprovider.api.dto.PaymentInitRequest;
+import com.rentauto.paymentprovider.api.dto.PaymentInitResponse;
+import com.rentauto.paymentprovider.plugins.PaymentPlugin;
+import com.rentauto.paymentprovider.service.TransactionService;
+
+import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
 public class QrCodePaymentPlugin implements PaymentPlugin {
 
+    private final TransactionService transactionService;
     private final QrGenerator qrGenerator;
 
     @Override
